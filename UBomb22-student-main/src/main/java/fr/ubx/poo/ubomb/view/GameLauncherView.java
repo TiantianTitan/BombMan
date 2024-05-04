f9248876915f83309d86010ef313868b7504f48d
Haotian XUE <haotianx4@gmail.com>
2024-05-04 16:29:20 +0200

    change fenete

--- UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/view/GameLauncherView.java
+++ UBomb22-student-main/src/main/java/fr/ubx/poo/ubomb/view/GameLauncherView.java
@@ -29,7 +29,7 @@ public class GameLauncherView extends BorderPane {
         MenuBar menuBar = new MenuBar();
         Menu menuFile = new Menu("File");
         MenuItem loadItem = new MenuItem("Load from file ...");
-        MenuItem defaultItem = new MenuItem("Load default configuration");
+        MenuItem defaultItem = new MenuItem("SinglePlayerMode");
         MenuItem scoreModeItem = new MenuItem("scoreMode");
         MenuItem PlayersModeItem = new MenuItem("2PlayersMode");
         MenuItem exitItem = new MenuItem("Exit");
@@ -115,11 +115,72 @@ public class GameLauncherView extends BorderPane {
 
 
         defaultItem.setOnAction(e -> {
-            Game game = GameLauncher.load();
-            modeScore = false;
-            mode2Players = false;
-            GameEngine engine = new GameEngine(game, stage,1,modeScore,mode2Players);
-            engine.start();
+            String filePath = "world/NewWorld.properties";
+            File file = new File(filePath);
+            if (file.exists()) {
+                // Implementation de lire le fichier
+                java.util.Properties config = new Properties();;
+                Reader in = null;
+                try {
+                    in = new FileReader(file);
+                } catch (FileNotFoundException ex) {
+                    throw new RuntimeException(ex);
+                }
+                try {
+                    config.load(in);
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                } finally {
+                    try {
+                        if (in != null) {
+                            in.close();
+                        }
+                    } catch (IOException ex) {
+                        ex.printStackTrace();
+                    }
+                }
+        
+                // Les varaibles qu'on a enregistré
+                boolean compression = Boolean.parseBoolean(config.getProperty("compression"));
+                int levels = Integer.parseInt(config.getProperty("levels"));
+                int playerLives = Integer.parseInt(config.getProperty("playerLives"));
+                int bombBagCapacity = Integer.parseInt(config.getProperty("bombBagCapacity"));
+                int monsterVelocity = Integer.parseInt(config.getProperty("monsterVelocity"));
+                int playerInvisibilityTime = Integer.parseInt(config.getProperty("playerInvisibilityTime"));
+                int monsterInvisibilityTime = Integer.parseInt(config.getProperty("monsterInvisibilityTime"));
+        
+                // Success for the player position initial '0x0' '1x1' '12x5'
+                StringBuffer tmpx = new StringBuffer();
+                StringBuffer tmpy = new StringBuffer();
+                int ind = 0;
+                while(config.getProperty("player").charAt(ind) != 'x'){
+                    tmpx.append(config.getProperty("player").charAt(ind));
+                    ind++;
+                }
+                ind++;
+                for(int i = ind; i <config.getProperty("player").length();i++ ){
+                    tmpy.append(config.getProperty("player").charAt(i));
+                }
+                int xPlayer = Integer.parseInt(tmpx.toString());
+                int yPlayer = Integer.parseInt(tmpy.toString());
+        
+                // For the detail of the maps
+                String string = config.getProperty("level1");
+                Configuration configuration = new Configuration(new Position(xPlayer,yPlayer),bombBagCapacity,playerLives,playerInvisibilityTime,monsterVelocity,monsterInvisibilityTime);
+                Game game = new Game(configuration,new Level(GameLauncher.load(string)));
+                // Save all maps in the game
+                game.stringMaps = new String[levels];
+                for(int i = 0; i < levels; i++){
+                    game.stringMaps[i] = config.getProperty("level"+(i+1));
+                }
+                modeScore = false;
+                mode2Players = false;
+                GameEngine engine = new GameEngine(game, stage,1,modeScore,mode2Players);
+                engine.start();
+        
+            } else {
+                System.out.println("File does not exist: " + filePath);
+            }
         });
 
         // ScoreMode
