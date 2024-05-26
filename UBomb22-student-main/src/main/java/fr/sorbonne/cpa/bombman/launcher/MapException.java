dff1bfb0326a7750ade9551e29d1b3c249b4d2e5
Haotian XUE <haotianx4@gmail.com>
2024-05-26 14:02:31 +0200

    2zips

--- UBomb22-student-main/src/main/java/fr/sorbonne/cpa/bombman/launcher/MapException.java
+++
@@ -1,7 +0,0 @@
-package fr.sorbonne.cpa.bombman.launcher;
-
-public class MapException extends RuntimeException {
-    public MapException(String message) {
-        super(message);
-    }
-}
--- UBomb22-student-main/src/main/java/fr/sorbonne/cpa/bombman/launcher/MapRepo.java
+++
@@ -1,8 +0,0 @@
-package fr.sorbonne.cpa.bombman.launcher;
-
-public interface MapRepo {
-
-     MapLevel load(String string);
-
-    String export(MapLevel mapLevel);
-}
